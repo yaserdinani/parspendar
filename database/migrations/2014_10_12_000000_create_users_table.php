64b2be62b9000c367ef6a78afa81943cbb0cb5b0
yaserdinani <yaserdinani1999@gmail.com>
2022-07-14 13:07:49 +0430

    + refactor user model file + refactor user migration file

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,9 +17,10 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('phone')->unique();
             $table->string('password');
-            $table->rememberToken();
+            $table->boolean('is_active')->default(true);
+            $table->boolean('is_admin')->default(false);
             $table->timestamps();
         });
     }
