0b4dcf4429a710a189226b40e2425180c7babd96
yaserdinani <yaserdinani1999@gmail.com>
2022-07-15 21:28:14 +0430

    + show tasks of user

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -56,6 +56,69 @@
                 </tr>
             </tbody>
         </table>
+        <hr>
+        <div class="text-center">
+            <h3>{{$user->name}} وظایف</h3>
+        </div>
+        <hr>
+        <table class="table table-bordered" dir="rtl">
+            <thead>
+              <tr>
+                <th scope="col">شناسه</th>
+                <th scope="col">نام</th>
+                <th scope="col">توضیحات</th>
+                <th scope="col">وضعیت</th>
+                <th scope="col">تاریخ شروع</th>
+                <th scope="col">تاریخ پایان</th>
+                <th scope="col">ویرایش</th>
+                <th scope="col">نمایش</th>
+                <th scope="col">حذف</th>
+              </tr>
+            </thead>
+            <tbody>
+              @foreach($user->tasks()->get() as $task)
+                <tr>
+                    <th scope="row">{{$task->id}}</th>
+                    <td>{{$task->name}}</td>
+                    <td>{{$task->description}}</td>
+                    <td>
+                        @switch($task->status)
+                            @case(0)
+                                <span class="text-danger">انجام نشده</span>
+                                @break
+                            @case(1)
+                                <span class="text-primary">در حال انجام</span>
+                                @break
+                            @case(2)
+                                <span class="text-success">انجام شده</span>
+                                @break
+                            @default
+                                <span class="text-danger">انجام نشده</span>
+                        @endswitch
+                    </td>
+                    <td>
+                        {{\Morilog\Jalali\Jalalian::forge($task->started_at)->format('%A %d %B %Y')}}
+                    </td>
+                    <td>
+                        {{\Morilog\Jalali\Jalalian::forge($task->finished_at)->format('%A %d %B %Y')}}
+                    </td>
+                    <td>
+                        <a href="{{route('tasks.edit',$task)}}" class="btn btn-sm btn-outline-warning">ویرایش</a>
+                    </td>
+                    <td>
+                        <a href="{{route('tasks.show',$task)}}" class="btn btn-sm btn-outline-info">نمایش</a>
+                    </td>
+                    <td>
+                        <form action="{{route('tasks.destroy',$task)}}" method="post">
+                            @csrf
+                            @method('delete')
+                            <button type="submit" class="btn btn-sm btn-outline-danger">حذف</button>
+                        </form>
+                    </td>
+                </tr>
+              @endforeach
+            </tbody>
+        </table>
     </div>
 </div>
 @endsection
