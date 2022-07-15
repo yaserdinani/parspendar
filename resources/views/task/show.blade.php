2fd46ad0e9d59a82ceb8e8517a970a2f0f5af940
yaserdinani <yaserdinani1999@gmail.com>
2022-07-15 21:23:48 +0430

    + show users of task

--- resources/views/task/show.blade.php
+++ resources/views/task/show.blade.php
@@ -62,6 +62,67 @@
                 </tr>
             </tbody>
         </table>
+        <hr>
+        <div class="text-center">
+            افراد مشارکت کننده
+        </div>
+        <hr>
+        <table class="table table-bordered" dir="rtl">
+            <thead>
+              <tr>
+                <th scope="col">شناسه</th>
+                <th scope="col">نام و نام خانوادگی</th>
+                <th scope="col">شماره تماس</th>
+                <th scope="col">ایمیل</th>
+                <th scope="col">نقش</th>
+                <th scope="col">وضعیت</th>
+                @if(auth()->user()->is_admin)
+                    <th scope="col">ویرایش</th>
+                    <th scope="col">نمایش</th>
+                    <th scope="col">حذف</th>
+                @endif
+              </tr>
+            </thead>
+            <tbody>
+              @foreach($task->users()->get() as $user)
+                <tr>
+                    <th scope="row">{{$user->id}}</th>
+                    <td>{{$user->name}}</td>
+                    <td>{{$user->phone}}</td>
+                    <td>{{$user->email}}</td>
+                    <td>
+                        @if($user->is_admin)
+                            مدیر
+                        @else
+                            کاربر
+                        @endif
+                    </td>
+                    <td>
+                        @if($user->is_active)
+                            <a href="" class="btn btn-sm btn-outline-success">فعال</a>
+                        @else
+                            <a href="" class="btn btn-sm btn-outline-danger">غیرفعال</a>
+                        @endif
+                    </td>
+                    @if(auth()->user()->is_admin)
+                        <td>
+                            <a href="{{route('users.edit',$user)}}" class="btn btn-sm btn-outline-warning">ویرایش</a>
+                        </td>
+                        <td>
+                            <a href="{{route('users.show',$user)}}" class="btn btn-sm btn-outline-info">نمایش</a>
+                        </td>
+                        <td>
+                            <form action="{{route('users.destroy',$user)}}" method="post">
+                                @csrf
+                                @method('delete')
+                                <button type="submit" class="btn btn-sm btn-outline-danger">حذف</button>
+                            </form>
+                        </td>
+                    @endif
+                </tr>
+              @endforeach
+            </tbody>
+        </table>
     </div>
 </div>
 @endsection
