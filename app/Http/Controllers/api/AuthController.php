8e9dc4d4c5f10d97af9cb1d4769f457bf2742cfc
yaserdinani <yaserdinani1999@gmail.com>
2022-07-17 13:17:50 +0430

    + debug auth api

--- app/Http/Controllers/api/AuthController.php
+++ app/Http/Controllers/api/AuthController.php
@@ -5,48 +5,58 @@ namespace App\Http\Controllers\api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use JWTAuth;
+use Validator;
 
 class AuthController extends Controller
 {
     public function login(Request $request){
-        $result = $this->validate($request,[
+        $validator = Validator::make($request->all(),[
             'phone'=>['required','regex:/^(\+98|0)?9\d{9}$/u'],
             'password'=>['required']
         ]);
 
-        if($result){
-            if(!$token = JWTAuth::attempt($request->only('phone','password'))){
-                return ["success"=>false,"message"=>"نام کاربری یا رمز عبور اشتباه است"];
-            }
-    
-            else{
-                return ["success"=>true,"message"=>"شما با موفقیت وارد شدید", "token"=>$token];
-            }
+        if($validator->fails()) {
+            return response([
+                "success"=>false,
+                "meesage"=>"اطلاعات ورودی نامعتبر است",
+                "errors"=>$validator->errors()->all()
+            ],400);
         }
         else{
-            return [
-                "sucees"=>false,
-                "message"=>"اطلاعات ورودی نامعتبر است"
-            ];
+
+            if(!$token = JWTAuth::attempt($request->only('phone','password'))){
+                return response([
+                    "success"=>false,
+                    "message"=>"نام کاربری یا رمز عبور اشتباه است"
+                ],406);
+            }
+            else{
+                return response([
+                    "success"=>true,
+                    "message"=>"شما با موفقیت وارد شدید",
+                    "token"=>$token
+                ],200);
+            }
         }
     }
 
     public function logout(Request $request){
-        $result = $this->validate($request,[
+        $validator = Validator::make($request->all(),[
             'token'=>['required']
         ]);
-        if($result){
-            JWTAuth::invalidate($request->token);
-            return [
-                "sucees"=>true,
-                "message"=>"شما با موفقیت خارج شدید"
-            ];
+        if($validator->fails()){
+            return response([
+                "success"=>false,
+                "meesage"=>"اطلاعات ورودی نامعتبر است",
+                "errors"=>$validator->errors()->all()
+            ],400);
         }
         else{
-            return [
-                "sucees"=>false,
-                "message"=>"اطلاعات ورودی نامعتبر است"
-            ];
+            JWTAuth::invalidate($request->token);
+            return response([
+                "sucees"=>true,
+                "message"=>"شما با موفقیت خارج شدید"
+            ],200);
         }
     }
 }
