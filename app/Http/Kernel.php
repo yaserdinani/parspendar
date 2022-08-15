a99b24dc3ef3ccbf19aa754f98c90e77350e50e5
yaserdinani <yaserdinani1999@gmail.com>
2022-08-15 09:04:24 +0430

    + install spatie library to project + publish needed spatie files to project + run php artisan migrate command

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -67,5 +67,8 @@ class Kernel extends HttpKernel
         'jwt.verify' => \App\Http\Middleware\Jwt::class,
         'jwt.auth' => 'Tymon\JWTAuth\Middleware\GetUserFromToken',
         'jwt.refresh' => 'Tymon\JWTAuth\Middleware\RefreshToken',
+        'role' => \Spatie\Permission\Middlewares\RoleMiddleware::class,
+        'permission' => \Spatie\Permission\Middlewares\PermissionMiddleware::class,
+        'role_or_permission' => \Spatie\Permission\Middlewares\RoleOrPermissionMiddleware::class,
     ];
 }
