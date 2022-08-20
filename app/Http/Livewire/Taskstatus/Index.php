fc02f039e44abdc46079792e3520e5934702e99f
yaserdinani <yaserdinani1999@gmail.com>
2022-08-20 13:35:08 +0430

    + add alert for task status model + refactor task status files

--- app/Http/Livewire/Taskstatus/Index.php
+++ app/Http/Livewire/Taskstatus/Index.php
@@ -5,13 +5,13 @@ namespace App\Http\Livewire\Taskstatus;
 use Livewire\Component;
 use App\Models\TaskStatus;
 use Livewire\WithPagination;
+use Jantinnerezo\LivewireAlert\LivewireAlert;
 
 class Index extends Component
 {
-    use WithPagination;
+    use WithPagination,LivewireAlert;
 
     protected $paginationTheme = 'bootstrap';
-    // public $statuses;
     public $name;
     public $status_id;
     public $update_mode = false;
@@ -19,19 +19,23 @@ class Index extends Component
     public $filter_text;
     public $color;
 
-    // protected $listeners = ["statusAdded","statusChanged","statusRemoved"];
+    protected $listeners =["changeStatusColor"];
 
     protected $rules = [
         "name"=>["required","string","min:3","max:30"],
         "color"=>["required"]
     ];
 
-    public function mount(){
-        // $this->statuses = TaskStatus::all();
-    }
-
     public function setStatus(TaskStatus $status){
         $this->current_status = $status;
+    }
+
+    public function changeStatusColor(TaskStatus $status,$value){
+        abort_unless(auth()->user()->can('status-edit'), '403', 'Unauthorized.');
+        $status->update([
+            "color"=>$value
+        ]);
+        $this->alert('success', 'رنگ وضعیت ویرایش شد');
     }
 
     public function resetInputs(){
@@ -39,26 +43,16 @@ class Index extends Component
         $this->update_mode = false;
         $this->status_id = null;
         $this->current_status = null;
+        $this->color= null;
+        $this->resetValidation();
     }
-
-    // public function statusAdded(){
-    //     $this->statuses = TaskStatus::all();
-    // }
-
-    // public function statusChanged(){
-    //     $this->statuses = TaskStatus::all();
-    // }
-
-    // public function statusRemoved(){
-    //     $this->statuses = TaskStatus::all();
-    // }
 
     public function store(){
         abort_unless(auth()->user()->can('status-create'), '403', 'Unauthorized.');
         $validation = $this->validate();
         TaskStatus::create($validation);
-        // $this->emit('statusAdded');
         $this->resetInputs();
+        $this->alert('success', 'وضعیت جدید ایجاد شد');
     }
 
     public function edit(TaskStatus $status){
@@ -66,6 +60,7 @@ class Index extends Component
         $this->status_id = $status->id;
         $this->name = $status->name;
         $this->current_status = $status;
+        $this->color = $status->color;
     }
 
     public function update(){
@@ -74,8 +69,7 @@ class Index extends Component
 
         if($this->status_id){
             $this->current_status->update($validation);
-            $this->resetInputs();
-            // $this->emit('statusChanged');
+            $this->alert('success', 'وضعیت ویرایش شد');
         }
 
     }
@@ -84,7 +78,7 @@ class Index extends Component
         abort_unless(auth()->user()->can('status-delete'), '403', 'Unauthorized.');
         $this->current_status->delete();
         $this->resetInputs();
-        // $this->emit('statusRemoved');
+        $this->alert('success', 'وضعیت به سطل زباله انتقال یافت');
     }
 
     public function render()
@@ -92,7 +86,7 @@ class Index extends Component
         abort_unless(auth()->user()->can('status-list'), '403', 'Unauthorized.');
         $statuses = TaskStatus::select("*")
         ->where('name',"LIKE","%".$this->filter_text."%")
-        ->paginate(2);
+        ->paginate(5);
         return view('livewire.taskstatus.index',["statuses"=>$statuses]);
     }
 }
--- resources/views/livewire/taskstatus/index.blade.php
+++ resources/views/livewire/taskstatus/index.blade.php
@@ -6,8 +6,8 @@
             <div>
                 <a data-toggle="modal" data-target="#createModal" class="btn btn-success">افزودن</a>
             </div>
-            <input autocomplete="off" type="text" class="col-md-2 form-control text-right mx-1" wire:model.lazy='filter_text'
-                    placeholder="جستجو" id="filter_text" name="filter_text">
+            <input autocomplete="off" type="text" class="col-md-2 form-control text-right mx-1"
+                wire:model.lazy='filter_text' placeholder="جستجو" id="filter_text" name="filter_text">
         </div>
     </div>
     <div class="card-body">
@@ -16,11 +16,12 @@
                 <tr>
                     <th scope="col">شناسه</th>
                     <th scope="col">عنوان</th>
+                    <th scope="col">رنگ</th>
                     @can('status-edit')
-                    <th scope="col">ویرایش</th>
+                        <th scope="col">ویرایش</th>
                     @endcan
                     @can('status-delete')
-                    <th scope="col">حذف</th>
+                        <th scope="col">حذف</th>
                     @endcan
                 </tr>
             </thead>
@@ -29,17 +30,20 @@
                     <tr>
                         <th scope="row">{{ $status->id }}</th>
                         <td>{{ $status->name }}</td>
-                        @can('status-edit')
                         <td>
-                            <a data-toggle="modal" data-target="#updateModal" class="btn btn-sm btn-outline-warning"
-                                wire:click="edit({{ $status }})">ویرایش</a>
+                            <input type="color" value="{{$status->color}}" wire:change="$emit('changeStatusColor',{{$status}},$event.target.value)">
                         </td>
+                        @can('status-edit')
+                            <td>
+                                <a data-toggle="modal" data-target="#updateModal" class="btn btn-sm btn-outline-warning"
+                                    wire:click="edit({{ $status }})">ویرایش</a>
+                            </td>
                         @endcan
                         @can('status-delete')
-                        <td>
-                            <a data-toggle="modal" data-target="#deleteModal" class="btn btn-sm btn-outline-danger"
-                                wire:click="setStatus({{ $status }})">حذف</a>
-                        </td>
+                            <td>
+                                <a data-toggle="modal" data-target="#deleteModal" class="btn btn-sm btn-outline-danger"
+                                    wire:click="setStatus({{ $status }})">حذف</a>
+                            </td>
                         @endcan
                     </tr>
                 @endforeach
@@ -59,14 +63,29 @@
                     </button>
                 </div>
                 <div class="modal-body">
-                    <input type="text" class="form-control" placeholder="عنوان" wire:model.lazy='name' required>
-                    @error('name')
-                        <div class="alert alert-danger">{{ $message }}</div>
-                    @enderror
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-outline-secondary" data-dismiss="modal">لغو</button>
-                    <button type="button" class="btn btn-outline-primary" wire:click='store' data-dismiss="modal">ثبت</button>
+                    <form wire:submit.prevent='store'>
+                        <div class="form-row">
+                            <div class="form-group col-md-6 text-right">
+                                <label for="name">عنوان</label>
+                                <input type="text" class="form-control text-right" wire:model.lazy='name'
+                                    id="name" placeholder="عنوان" required autocomplete="off">
+                                @error('name')
+                                    <div class="alert alert-danger">{{ $message }}</div>
+                                @enderror
+                            </div>
+                            <div class="form-group col-md-6 text-right">
+                                <label for="color">رنگ</label>
+                                <input type="color" class="form-control text-right" wire:model.lazy='color'
+                                    id="color" required placeholder="رنگ" autocomplete="off">
+                                @error('color')
+                                    <div class="alert alert-danger">{{ $message }}</div>
+                                @enderror
+                            </div>
+                        </div>
+                        <button type="button" class="btn btn-outline-secondary" data-dismiss="modal"
+                            wire:click="resetInputs">لغو</button>
+                        <button type="submit" class="btn btn-outline-primary">ثبت</button>
+                    </form>
                 </div>
             </div>
         </div>
@@ -83,14 +102,29 @@
                     </button>
                 </div>
                 <div class="modal-body">
-                    <input type="text" class="form-control" placeholder="عنوان" wire:model.lazy='name' required>
-                    @error('name')
-                        <div class="alert alert-danger">{{ $message }}</div>
-                    @enderror
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-outline-secondary" data-dismiss="modal">لغو</button>
-                    <button type="button" class="btn btn-outline-primary" wire:click='update' data-dismiss="modal">ثبت</button>
+                    <form wire:submit.prevent='update'>
+                        <div class="form-row">
+                            <div class="form-group col-md-6 text-right">
+                                <label for="name">عنوان</label>
+                                <input type="text" class="form-control text-right" wire:model.lazy='name'
+                                    id="name" placeholder="عنوان" required autocomplete="off">
+                                @error('name')
+                                    <div class="alert alert-danger">{{ $message }}</div>
+                                @enderror
+                            </div>
+                            <div class="form-group col-md-6 text-right">
+                                <label for="color">رنگ</label>
+                                <input type="color" class="form-control text-right" wire:model.lazy='color'
+                                    id="color" required placeholder="رنگ" autocomplete="off">
+                                @error('color')
+                                    <div class="alert alert-danger">{{ $message }}</div>
+                                @enderror
+                            </div>
+                        </div>
+                        <button type="button" class="btn btn-outline-secondary" data-dismiss="modal"
+                            wire:click="resetInputs">لغو</button>
+                        <button type="submit" class="btn btn-outline-primary">ثبت</button>
+                    </form>
                 </div>
             </div>
         </div>
