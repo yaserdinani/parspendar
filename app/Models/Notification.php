afd71c18e08292968f348fa0e155361d46c95ebc
yaserdinani <yaserdinani1999@gmail.com>
2022-08-21 14:59:03 +0430

    + add is_seen column to notifications table + refactor Notification model

--- app/Models/Notification.php
+++ app/Models/Notification.php
@@ -11,7 +11,8 @@ class Notification extends Model
 
     protected $fillable = [
         "user_id",
-        "description"
+        "description",
+        "is_seen"
     ];
 
     public function user(){
